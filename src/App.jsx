731215fd8d06efac505346b826fca21951a69562
YergaliKumisbay <kumisbayyergali@gmail.com>
2025-01-27 19:44:05 +0500

    fsddfs

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Button from "./components/Button/Button";
+
+function App() {
+    return (
+        <>
+            <h1>Title</h1>
+            <p>text</p>
+            <Button/>
+        </>
+    );
+}
+
+export default App;
