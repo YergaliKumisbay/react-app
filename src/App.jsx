f2bb059d904bf7fa2831470847b87f3bfe74af33
YergaliKumisbay <kumisbayyergali@gmail.com>
2025-01-30 15:58:45 +0500

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,51 @@
 import './App.css';
-import Button from "./components/Button/Button";
+import JournalItem from "./components/JournalItem/JournalItem";
+import CardButton from "./components/CardButton/CardButton";
+import LeftPanel from "./layouts/LeftPanel/LeftPanel";
+import Header from "./components/Header/Header";
+import JournalList from "./components/JournalList/JournalList";
+import Body from "./layouts/Body/Body";
+import JournalAddButton from "./components/JournalAddButton/JournalAddButton.jsx";
 
 function App() {
+    const data = [
+        {
+            title: 'CardButton',
+            text: 'Lorem Ipsum the sfdfs sfs ffsegsrgsgggg',
+            date: new Date()
+        },
+        {
+            title: 'Journal',
+            text: 'Lorem Ipsum',
+            date: new Date()
+        }
+    ];
     return (
-        <>
-            <h1>Title</h1>
-            <p>текст</p>
-            <Button/>
-        </>
+        <div className="app">
+            <LeftPanel>
+                <Header/>
+                <JournalAddButton/>
+                <JournalList>
+                    <CardButton>
+                        <JournalItem
+                            title={data[0].title}
+                            text={data[0].text}
+                            date={data[0].date}
+                        />
+                    </CardButton>
+                    <CardButton>
+                        <JournalItem
+                            title={data[1].title}
+                            text={data[1].text}
+                            date={data[1].date}
+                        />
+                    </CardButton>
+                </JournalList>
+            </LeftPanel>
+            <Body>
+                body
+            </Body>
+        </div>
     );
 }
 
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,17 @@
 import './Button.css';
+import { useState } from 'react';
 
 function Button() {
+    // let text = 'Сохранить';
+    const [text, setText] = useState('Сохранить');
+    console.log('ререндер');
+    const clicked = () => {
+        setText(t => t + '!');
+        console.log(text);
+    };
 
     return (
-            <button className='button accent'>Сохранить</button>
+            <button onClick={clicked} className='button accent'>{text}</button>
     );
 }
 
